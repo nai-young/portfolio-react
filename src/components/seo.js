5cca01cc6ea7fd4d2b3a758700a75ff68cb04009
N Young <60322086+nai-young@users.noreply.github.com>
2021-08-09 13:48:12 +0100

    SEO file name

--- src/components/seo.js
+++ src/components/seo.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-function SEO ({ title, description, meta, lang }) {
+export default function SEO ({ title, description, meta, lang }) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -77,5 +77,3 @@ SEO.propTypes = {
   meta: PropTypes.arrayOf(PropTypes.object),
   title: PropTypes.string.isRequired,
 }
-
-export default SEO
